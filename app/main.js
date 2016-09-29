f8ae834f799dca9a2ab1e2552bf0118b1809bdc3
victor.feijo <victor.feijo@oi.victor.feijo.vms.ufsc.br>
2016-09-29 11:11:20 -0300

    fetching the form data

--- app/main.js
+++ app/main.js
@@ -1,6 +1,91 @@
 import Simulator from './Simulator'
 
-$('#alert').click(() => { 
+let trafficVolumn = () => {
+  return {
+    ll: $('#trafficVolumnLL').val(),
+    rr: $('#trafficVolumnRR').val(),
+    lr: $('#trafficVolumnLR').val(),
+    rl: $('#trafficVolumnRL').val()
+  }
+}
+
+let sfaTaxs = () => {
+  return {
+    sucess: {
+      ll: $('#sTaxsLL').val(),
+      rr: $('#sTaxsRR').val(),
+      lr: $('#sTaxsLR').val(),
+      rl: $('#sTaxsRL').val(),
+    },
+    failure: {
+      ll: $('#fTaxsLL').val(),
+      rr: $('#fTaxsRR').val(),
+      lr: $('#fTaxsLR').val(),
+      rl: $('#fTaxsRL').val(),
+    },
+    indentation: {
+      ll: $('#aTaxsLL').val(),
+      rr: $('#aTaxsRR').val(),
+      lr: $('#aTaxsLR').val(),
+      rl: $('#aTaxsRL').val(),
+    }
+  }
+}
+
+let serviceCenter = () => {
+  return {
+    center1: $('#serviceCenter1').val(),
+    center2: $('#serviceCenter2').val()
+  }
+}
+
+let arriveTime = () => {
+  return {
+    local: $('#arriveTime1').val(),
+    remote: $('#arriveTime2').val()
+  }
+}
+
+let serviceTime = () => {
+  return {
+    reception: {
+      lls: $('#receptionCenterLLS').val(),
+      llf: $('#receptionCenterLLF').val(),
+      lla: $('#receptionCenterLLA').val(),
+      lrs: $('#receptionCenterLRS').val(),
+      lrf: $('#receptionCenterLRF').val(),
+      lra: $('#receptionCenterLRA').val(),
+      rls: $('#receptionCenterRLS').val(),
+      rlf: $('#receptionCenterRLF').val(),
+      rla: $('#receptionCenterRLA').val(),
+      rrs: $('#receptionCenterRRS').val(),
+      rrf: $('#receptionCenterRRF').val(),
+      rra: $('#receptionCenterRRA').val(),
+    },
+    service: {
+      lls: $('#serviceCenterLLS').val(),
+      llf: $('#serviceCenterLLF').val(),
+      lla: $('#serviceCenterLLA').val(),
+      lrs: $('#serviceCenterLRS').val(),
+      lrf: $('#serviceCenterLRF').val(),
+      lra: $('#serviceCenterLRA').val(),
+      rls: $('#serviceCenterRLS').val(),
+      rlf: $('#serviceCenterRLF').val(),
+      rla: $('#serviceCenterRLA').val(),
+      rrs: $('#serviceCenterRRS').val(),
+      rrf: $('#serviceCenterRRF').val(),
+      rra: $('#serviceCenterRRA').val(),
+    }
+  }
+}
+
+$('#alert').click(() => {
   let sim = new Simulator()
   sim.start()
+
+  console.log(trafficVolumn())
+  console.log(sfaTaxs())
+  console.log(serviceCenter())
+  console.log(arriveTime())
+  console.log(serviceTime())
 })
--- public/bundle.js
+++ public/bundle.js
@@ -52,9 +52,98 @@
 	
 	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 	
+	var test = 10;
+	
+	var trafficVolumn = function trafficVolumn() {
+	  return {
+	    ll: $('#trafficVolumnLL').val(),
+	    rr: $('#trafficVolumnRR').val(),
+	    lr: $('#trafficVolumnLR').val(),
+	    rl: $('#trafficVolumnRL').val()
+	  };
+	};
+	
+	var sfaTaxs = function sfaTaxs() {
+	  return {
+	    sucess: {
+	      ll: $('#sTaxsLL').val(),
+	      rr: $('#sTaxsRR').val(),
+	      lr: $('#sTaxsLR').val(),
+	      rl: $('#sTaxsRL').val()
+	    },
+	    failure: {
+	      ll: $('#fTaxsLL').val(),
+	      rr: $('#fTaxsRR').val(),
+	      lr: $('#fTaxsLR').val(),
+	      rl: $('#fTaxsRL').val()
+	    },
+	    indentation: {
+	      ll: $('#aTaxsLL').val(),
+	      rr: $('#aTaxsRR').val(),
+	      lr: $('#aTaxsLR').val(),
+	      rl: $('#aTaxsRL').val()
+	    }
+	  };
+	};
+	
+	var serviceCenter = function serviceCenter() {
+	  return {
+	    center1: $('#serviceCenter1').val(),
+	    center2: $('#serviceCenter2').val()
+	  };
+	};
+	
+	var arriveTime = function arriveTime() {
+	  return {
+	    local: $('#arriveTime1').val(),
+	    remote: $('#arriveTime2').val()
+	  };
+	};
+	
+	var serviceTime = function serviceTime() {
+	  return {
+	    reception: {
+	      lls: $('#receptionCenterLLS').val(),
+	      llf: $('#receptionCenterLLF').val(),
+	      lla: $('#receptionCenterLLA').val(),
+	      lrs: $('#receptionCenterLRS').val(),
+	      lrf: $('#receptionCenterLRF').val(),
+	      lra: $('#receptionCenterLRA').val(),
+	      rls: $('#receptionCenterRLS').val(),
+	      rlf: $('#receptionCenterRLF').val(),
+	      rla: $('#receptionCenterRLA').val(),
+	      rrs: $('#receptionCenterRRS').val(),
+	      rrf: $('#receptionCenterRRF').val(),
+	      rra: $('#receptionCenterRRA').val()
+	    },
+	    service: {
+	      lls: $('#serviceCenterLLS').val(),
+	      llf: $('#serviceCenterLLF').val(),
+	      lla: $('#serviceCenterLLA').val(),
+	      lrs: $('#serviceCenterLRS').val(),
+	      lrf: $('#serviceCenterLRF').val(),
+	      lra: $('#serviceCenterLRA').val(),
+	      rls: $('#serviceCenterRLS').val(),
+	      rlf: $('#serviceCenterRLF').val(),
+	      rla: $('#serviceCenterRLA').val(),
+	      rrs: $('#serviceCenterRRS').val(),
+	      rrf: $('#serviceCenterRRF').val(),
+	      rra: $('#serviceCenterRRA').val()
+	    }
+	  };
+	};
+	
 	$('#alert').click(function () {
 	  var sim = new _Simulator2.default();
 	  sim.start();
+	
+	  console.log(test);
+	
+	  console.log(trafficVolumn());
+	  console.log(sfaTaxs());
+	  console.log(serviceCenter());
+	  console.log(arriveTime());
+	  console.log(serviceTime());
 	});
 
 /***/ },
@@ -96,6 +185,7 @@
 	    value: function start() {
 	      var currentTime = 0;
 	
+	      console.log(test);
 	      this.generateEvents();
 	
 	      this.eventQueue.forEach(function (event, i) {
