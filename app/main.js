ed79d8c3a250bd5421843762b1eb478cbf9468bf
Victor Feijo <victor.feijoa@gmail.com>
2016-10-20 22:20:19 -0200

    added smooth scroll down

--- app/main.js
+++ app/main.js
@@ -2,6 +2,22 @@ import Simulator from './Simulator'
 import SimulatorConfig from './SimulatorConfig'
 import { Distribution, parse } from './Calculus'
 
+// Smooth Scoll
+$(function() {
+  $('a[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+});
+
 let trafficVolumn = () => {
   return {
     ll: parseFloat($('#trafficVolumnLL').val()),
--- public/bundle.js
+++ public/bundle.js
@@ -58,6 +58,21 @@
 	
 	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 	
+	$(function () {
+	  $('a[href*="#"]:not([href="#"])').click(function () {
+	    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+	      var target = $(this.hash);
+	      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+	      if (target.length) {
+	        $('html, body').animate({
+	          scrollTop: target.offset().top
+	        }, 1000);
+	        return false;
+	      }
+	    }
+	  });
+	});
+	
 	var trafficVolumn = function trafficVolumn() {
 	  return {
 	    ll: parseFloat($('#trafficVolumnLL').val()),
